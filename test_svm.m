435898ce9f1c724bdece5593d51f1c3d920fbfc1
Ramona Imhof <mona.i95@icloud.com>
2017-04-05 13:26:34 +0200

    Added hog to test_svm.m

--- test_svm.m
+++ test_svm.m
@@ -11,6 +11,14 @@ train_labels = train(1 : n_data, 1);
 train_images = train(1 : n_data, 2 : end);
 clear train;
 
+%% Feature Selection/ Feature Descriptor
+for i=1:size(train_images,1)
+    hg=hog(reshape(train_images(i,:),28,28),8);
+    newData(i,:)= hg(:);
+end
+size(newData)
+train_images=newData;
+
 %%  Train model
 tic;
 model = svmtrain(train_labels, train_images);

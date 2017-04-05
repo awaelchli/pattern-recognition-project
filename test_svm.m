ad397e7b42b2bca318240c12a327c66c803c4753
Ramona Imhof <mona.i95@icloud.com>
2017-04-05 18:25:33 +0200

    added feature selection with splitting up the image in squares and summing up the pixels

--- test_svm.m
+++ test_svm.m
@@ -4,7 +4,7 @@ clc;
 clear;
 
 %% Load the training set
-fraction = 0.1;
+fraction = 1;
 train = csvread('data/train.csv');
 n_data = ceil(fraction * size(train, 1));
 train_labels = train(1 : n_data, 1);
@@ -12,12 +12,18 @@ train_images = train(1 : n_data, 2 : end);
 clear train;
 
 %% Feature Selection/ Feature Descriptor
+%HOG
 for i=1:size(train_images,1)
     hg=hog(reshape(train_images(i,:),28,28),8);
     newData(i,:)= hg(:);
 end
+size(train_images)
+%train_images=newData;
 size(newData)
-train_images=newData;
+
+%squares feature selection
+train_images=featureSelection(train_images,4);
+size(train_images)
 
 %%  Train model
 tic;

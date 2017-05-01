9de85e0ec91fe95812a965806fc3c5b73a824715
Mario Kaufmann <mario.kaufmann@bluemail.ch>
2017-05-01 09:42:41 +0200

    changed import statement

--- 03/preprocessing/partition_images.py
+++ 03/preprocessing/partition_images.py
@@ -2,7 +2,7 @@ import numpy as np
 import os
 import scipy.io as sio
 
-from preprocessing.cut_path import cut_path
+import cut_path as cp
 
 ground_truth_dir = '../data/ground-truth/locations/'
 files = os.listdir(ground_truth_dir)
@@ -10,7 +10,7 @@ nr_files = len(files)
 multiple_cut_words = []
 for idx, file in enumerate(files):
     file_number = file[0:3]
-    single_cut_word = cut_path(file_number, ground_truth_dir)
+    single_cut_word = cp.cut_path(file_number, ground_truth_dir)
     # also save filename
     multiple_cut_words.append([file, single_cut_word])
 
